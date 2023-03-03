312be8493c7888680e13642bf7f26613cdc45bb0
Caio Zendron <62120876+czendron@users.noreply.github.com>
2023-03-03 13:02:06 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 import json
 
 app = Flask(__name__)
@@ -9,12 +9,22 @@ def hello_world():
   jobs = load_jobs_from_db()
   return render_template('home.html', 
                          jobs=jobs,
-                         company_name='Jovian')
+                         )
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html',
+                        job=job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -20,3 +20,15 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(zip(column_names, row)))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = {id}")
+    )
+    column_names = result.keys()
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(zip(column_names, rows[0]))
